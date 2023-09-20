d3226714d4cf76475dc92d47a408b9950a1c63ea
MohitDhaker07 <mohit.dhaker04@gmail.com>
2023-09-20 18:28:47 +0530

    test-commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current year and update the footer
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+});
