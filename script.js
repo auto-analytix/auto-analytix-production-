fca64b9577062ffb0b055f85a0e8a43093a5a6d0
MohitDhaker07 <mohit.dhaker04@gmail.com>
2023-09-20 19:29:15 +0530

    driectory cleaned

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the current year and update the footer
-    const currentYear = new Date().getFullYear();
-    document.getElementById("currentYear").textContent = currentYear;
-});
